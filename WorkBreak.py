442ee3f06a693cfc4953a8dd468a4ce1e29ce261
Siam Amin <34121324+siamelixir@users.noreply.github.com>
2018-01-03 15:28:21 +0600

    The workbreak backend

---
+++ WorkBreak.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+count=0
+num_of_breaks=2
+time_intervals=60*60
+
+print ("This program started on "+time.ctime())
+while count<num_of_breaks:
+    time.sleep(time_intervals)
+    webbrowser.open("https://www.youtube.com/watch?v=4Tr0otuiQuU")
+    count=count+1
